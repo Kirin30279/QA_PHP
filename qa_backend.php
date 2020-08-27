5802856591f0d22b53869a29e36f07d9b32da780
DESKTOP-KLJVRMO\bibianuser <andy860826@japan-import.com.tw>
2020-08-27 18:48:54 +0800

    2020/08/27 18:48修正

--- qa_backend.php
+++ qa_backend.php
@@ -23,10 +23,10 @@
       <tbody>
         
         <?php
-        $servername='localhost';#主機名稱
-        $username='root';#使用者名稱
-        $password='';#使用者PW
-        $dbname = "day5_qa";#存取的DB名稱
+        $servername='localhost';//主機名稱
+        $username='root';//使用者名稱
+        $password='';//使用者PW
+        $dbname = "day5_qa";//存取的DB名稱
         $connect = new mysqli($servername,$username,$password,"$dbname");
         if ($connect->connect_error) {
            die("連線失敗: " . $connect->connect_error);

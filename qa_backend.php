bcce46098e97c7f7b7374ac8eb1b2cdc39385052
DESKTOP-KLJVRMO\bibianuser <andy860826@japan-import.com.tw>
2020-08-27 18:46:44 +0800

    2020/08/27 18:46 更新

--- qa_backend.php
+++ qa_backend.php
@@ -21,12 +21,7 @@
         </tr>
       </thead>
       <tbody>
-        <!-- <tr>
-          <td>1</td>
-          <td><a href="qa_backend_d.php?id=1234567890">1234567890</a></td>
-          <td>ListChen</td>
-          <td>2020-07-01 10:19:00</td>
-        </tr> -->
+        
         <?php
         $servername='localhost';#主機名稱
         $username='root';#使用者名稱
@@ -36,151 +31,78 @@
         if ($connect->connect_error) {
            die("連線失敗: " . $connect->connect_error);
         }
-        
-        //$oid = 7777777777;//這個訂單編號應該由外部傳入，從列表點選的時候固定
-        //$user_id = strval(8888);//這個客戶的id應該有方法可紀錄
-        
-        //var_dump(date("Y-m-d H:i:s"));
-        
-        $query = "SELECT * FROM `qa_list`  
-                  ORDER BY `status`ASC,`renew_time` DESC";
-                  //WHERE status = 1
-                  //這行可調，之後QA列表要想辦法送oid，才能查到該訂單編號的結果。
-                 //ORDER BY time ASC 資料隨著時間來升冪排列(遞增) 、DESC則是降冪
+        $query = 
+        "SELECT * FROM `qa_list`  
+        ORDER BY `status`ASC,`renew_time` DESC";
+        //後台列表優先顯示待處理訂單、以最後更新時間排序，後進先出
+
         $result = $connect->query($query);
-        if (!$result) die("Fatal Error");
-        
-        $rows = $result->num_rows;//符合qurey查找資格的行數有幾行，等等for迴圈要用
-        
-        for ($j = 0 ; $j < $rows ; ++$j)
-        {
-        $array = $result->fetch_array(MYSQLI_ASSOC);
-        //可以比較看看MYSQLI_NUM、MYSQLI_BOTH
-        
-        
-        $html  = '';
-        $html .= '<tr>';
-        $html .= '<td>';
-        $html .= $j+1;
-        $html .= '</td>';
-        $html .= '<td>';//提問超連結↓
-        $html .= '<a href="qa_backendwithMySQL.php?oid='.$array['oid'].'&'.'user_id='.$array['user_id'].'"'.'>'.$array['oid'].'</a></td>';
-        $html .= '<td>';//提問者名稱↓
-        $html .= $array['user_id'];
-        $html .= '</td>';
-        $html .= '<td>';//提問時間↓
-        $html .= $array['create_time'];
-        $html .= '</td>';
-        $html .= '<td>';//更新時間↓
-        $html .= $array['renew_time'];
-        $html .= '</td>';
-        $html .= '<td>';//問題類型↓ 費用問題、貨況問題、運送問題、其它
-        switch($array['type']){
-          case '1':
-            $html .= '費用';
-            $html .= '<img src="pic/money.png" alt="費用" title="費用" width="50px" height="50px">';
-          break;
-          case '2':
-            $html .= '貨況';
-            $html .= '<img src="pic/buy.png" alt="貨況" title="貨況" width="50px" height="50px">';
-          break;
-          case '3':
-            $html .= '運送';
-            $html .= '<img src="pic/truck.jpg" alt="運送" title="運送" width="50px" height="50px">';
-          break;
-          case '4':
-            $html .= '其它';
-            $html .= '<img src="pic/file.png" alt="其它" title="其它" width="50px" height="50px">';
-          break;
-          default:
-            $html .= '問題類型遺失';          
-          };
-       //$html .= $array['type'];
-        $html .= '</td>';
-        $html .= '<td>';//回覆狀況↓
-       //$html .= $array['status'];
-        switch($array['status']){
-          case '0':
-            //$html .= '待處理';
-            $html .= '<img src="pic/cross.png" alt="待處理" title="待處理" width="50px" height="50px">';
-          break;
-          case '1':
-            //$html .= '已完成';
-            $html .= '<img src="pic/check.jpg" alt="完成" title="完成" width="50px" height="50px">';
-          break;
-          default:
-            $html .= '回覆狀況遺失';          
-          };
-        $html .= '</td>';
-        $html .= '</tr>';
-        echo $html;
+        if (!$result) {//抓不到資料的話
+          die("Fatal Error");
         }
 
+        $rows = $result->num_rows;//符合qurey的資料有幾項
+      
+        for ($j = 0 ; $j < $rows ; ++$j){
+          $array = $result->fetch_array(MYSQLI_ASSOC);
+          $html  = '';
+          $html .= '<tr>';
+          $html .= '<td>';
+          $html .= $j+1;
+          $html .= '</td>';
+          $html .= '<td>';//提問超連結↓
+          $html .= '<a href="qa_backendwithMySQL.php?oid='.$array['oid'].'"'.'>'.$array['oid'].'</a></td>';
+          $html .= '<td>';//提問者名稱↓
+          $html .= $array['user_id'];
+          $html .= '</td>';
+          $html .= '<td>';//提問時間↓
+          $html .= $array['create_time'];
+          $html .= '</td>';
+          $html .= '<td>';//更新時間↓
+          $html .= $array['renew_time'];
+          $html .= '</td>';
+          $html .= '<td>';//問題類型↓ 費用問題、貨況問題、運送問題、其它
+          switch($array['type']){
+            case '1':
+              $html .= '費用';
+              $html .= '<img src="pic/money.png" alt="費用" title="費用" width="50px" height="50px">';
+            break;
+            case '2':
+              $html .= '貨況';
+              $html .= '<img src="pic/buy.png" alt="貨況" title="貨況" width="50px" height="50px">';
+            break;
+            case '3':
+              $html .= '運送';
+              $html .= '<img src="pic/truck.jpg" alt="運送" title="運送" width="50px" height="50px">';
+            break;
+            case '4':
+              $html .= '其它';
+              $html .= '<img src="pic/file.png" alt="其它" title="其它" width="50px" height="50px">';
+            break;
+            default:
+              $html .= '問題類型遺失';          
+          };
+          $html .= '</td>';
+          $html .= '<td>';//回覆狀況↓
+          switch($array['status']){
+            case '0':
+              //$html .= '待處理';
+              $html .= '<img src="pic/cross.png" alt="待處理" title="待處理" width="50px" height="50px">';
+            break;
+            case '1':
+              //$html .= '已完成';
+              $html .= '<img src="pic/check.jpg" alt="完成" title="完成" width="50px" height="50px">';
+            break;
+            default:
+              $html .= '回覆狀況遺失';
+          };
+          $html .= '</td>';
+          $html .= '</tr>';
+          echo $html;
+        }
         ?>
-
-        
-
       </tbody>
     </table>
   </div>
-
-  <script>
-    var temp = {
-          act: "checkCreatedInvoice",
-          data: {
-            "AV27913750": {
-              "Type":"2",
-              "InvDate":"2020/08/11",
-              "TaxId":" ",
-              "Title":" ",
-              "RandomCode":"4500",
-              "MemberId":58911,
-              "SubAmt":25,
-              "SubTaxAmt":0,
-              "SubNetAmt":25,
-              "CarriersType":"G0025",
-              "CarriersNo":" ",
-              "DonationCode":" ",
-              "Item": [ {
-                "DisplayLabel": "手續費",
-                "UnitPrice": 25,
-                "Quantity": 1,
-                "TaxAmount": 0,
-                "SaleAmount":25,
-                "SubtotalAmount":25 
-              }]
-            },
-            "AV27913761": {
-              "Type":"2",
-              "InvDate":"2020/08/11",
-              "TaxId":" ",
-              "Title":" ",
-              "RandomCode":"7869",
-              "MemberId":58339,
-              "SubAmt":124,
-              "SubTaxAmt":0,
-              "SubNetAmt":124,
-              "CarriersType":"G0025",
-              "CarriersNo":" ",
-              "DonationCode":" ",
-              "Item": [{ 
-                "DisplayLabel": "手續費",
-                "UnitPrice": 24,
-                "Quantity": 1,
-                "TaxAmount": 0,
-                "SaleAmount":24,
-                "SubtotalAmount":24 
-              }, { 
-                "DisplayLabel": "服務費",
-                "UnitPrice": 100,
-                "Quantity": 1,
-                "TaxAmount": 0,
-                "SaleAmount":100,
-                "SubtotalAmount":100
-              }]
-            }
-          }};
-    console.log(JSON.stringify(temp));
-  </script>
 </body>
 </html>
--- qa_fronttest.php
+++ qa_fronttest.php
@@ -7,28 +7,17 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
+
   <div class="m-2">
-    <!--用post上傳，用post下載
-    加上一個enctype="multipart/form-data，代表等等會上傳檔案
-    action=qa_control.php，意思是等等資料會傳到qa_control.php-->
-    <form action="qa_control_pic.php" method="post" enctype="multipart/form-data">
-   
-      
+    <form action="qa_control_pic.php" method="post" enctype="multipart/form-data">  
       <div class="form-group">
         <label>訂單編號(10位數字)</label>
         <input type="text" name="order_number" class="form-control" maxlength="10" pattern="[0-9]{10}" >
-        <!-- onkeydown="limit(this, 10)" -->
-        <!--text單行文本輸入
-        Q1:這裡是不是要name=order_number
-        input type="text" class="form-control" name="order_number"-->
       </div>
 
       <div class="form-group">
         <label>提問內容</label>
         <textarea name="Question" class="form-control" cols="30" ></textarea>
-        <!--Q2:這邊沒有input是不是要自己加，開一個textarea會直接被input送出來嗎？
-        //Q3:rows=?，Default值=5
-        //Q4:承Q1，name="Question"-->
       </div>
       
       <div class="form-group">
@@ -43,97 +32,39 @@
 
       <div class="form-group">
         <label>問題附件</label>
-        <!-- a button, trigger input file -->
-        <!-- 當每次上傳完, 怎紀錄在js變數中 -->
-        <!-- 如何限制跳出來的框框最多到三個 -->
-        <!-- <a href="#" onClick="createUploadBtn()">上傳檔案</a> -->
         <div id="mfiles">
         <input id="picture" name="PicFile[]" type="file" onchange="previewFiles()" accept="image/*" multiple>
-        <!-- <div id="preview"></div> -->
         </div>
-        <!-- 
-        <input type="file" name="PicFile[]" class="form-control" accept="image/*" onchange="loadFile(event,2)">  
-        <img id="output2" width="50" height="50"/>
-        <input type="file" name="PicFile[]" class="form-control" accept="image/*" onchange="loadFile(event,3)">  
-        <img id="output3" width="50" height="50"/> 
-        -->
       </div>
-        <!-- <a href="javascript:;" class="btn btn-primary">送出</a> <a href="javascript:;" class="btn btn-danger">取消</a> -->
-      
+
+      <div>
       <input type="submit" value='submit'>
       </div>
-    
-    </form>
-
-    
+    </form>  
   </div>
-
-  
-
 
   <script>
   function previewFiles() {
-  //Preview的工作區
-  var preview = document.querySelector('#preview');
-  //選取id元素名稱為preview的人，#代表抓id，.就是抓class
-  //以這邊為例子#preview其實會找到第43行的<div id="preview"></div>
-  var files   = document.querySelector('input[type=file]').files;
-  //同上，把第一個input[type=file]的人找出來，也就是說如果一次做三個single的input，這邊就只會抓到第一個input所上傳的file
-  //將找到第一個input[type=file]的人檔案內容抓出來，賦值給函數內的files。
-
-  console.log(files);
-
-  function readAndPreview(file) {
-
-    //底下這個if是拿來判斷傳入的檔案必須是img類型的檔案，不然沒辦法顯示，若判斷===1則進入if，
-    //若===0就不把它當圖片看了，實際上這個限制在input那邊限制成圖片檔案就可以不用這個f。
-    if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
-      var reader = new FileReader();
-      //新增變數reader，物件型態是FileReader，等等要拿來操作我們傳送進來的file
-      reader.addEventListener("load", function () {
-        var image = new Image();        //為了顯示圖片，我們建立一個新的image變數，等等要拿它來顯示圖片用(包括操作他的大小等等)
-        
-        
-        
-        
-        
-        
-        image.height = 100; //圖片限定大小為高度100px，自己調
-        image.alt = file.name;//圖片的title限定為傳進來的圖片名稱
-        image.src = this.result;//
-        preview.appendChild( image );
-      }, false);
-
-      reader.readAsDataURL(file);
+    var preview = document.querySelector('#preview');
+    var files   = document.querySelector('input[type=file]').files;
+    console.log(files);
+    function readAndPreview(file) {  
+      if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+        var reader = new FileReader();
+        reader.addEventListener("load", function () {
+          var image = new Image();   
+          image.height = 100; //圖片大小高度100px
+          image.alt = file.name;//圖片的title為傳進來的圖片名稱
+          image.src = this.result;
+          preview.appendChild( image );
+        }, false);
+        reader.readAsDataURL(file);
+      }
     }
-
-  }
-  //發現有檔案傳入就讓其Preview出來，因為我們前面寫的input是multiple的方式，
-  //只要判斷出有檔案傳入，以下的forEach會把所有本次傳入的檔案全部都跑一次readAndPreview。
-  if (files) {
-    [].forEach.call(files, readAndPreview);
-  }
-
-  }
-  </script>
-
-
-  <!-- <script src="uploadImageFile.js"></script>
-  //讀取uploadImageFile腳本 -->
-  <script>
-    var counter = 1;
-    var createUploadBtn = function() {
-      if (counter < 3){
-        counter = counter + 1;
-        let html = `<img id="output${counter}" width="50" height="50"/>`;
-        let sourceHtml = document.querySelector("#mfiles").innerHTML + html;
-        //讓輸出位置跟著count動為何無效
-        console.log(sourceHtml);
-
-        document.querySelector("#mfiles").innerHTML = sourceHtml;
-        
-      } 
+    if (files) {
+      [].forEach.call(files, readAndPreview);
     }
+  }
   </script>
 </body>
 </html>
